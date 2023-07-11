cdc10a6b026afadf0d34466905a7a273c5335c1b
[LucasA] <[lucas.anachuri@gmail.com]>
2023-07-10 21:34:55 -0300

    creacion de contador

---
+++ src/components/Titulos.jsx
@@ -0,0 +1,14 @@
+const Titulos = () => {
+  //aqui vamos a cargar toda la logica
+  return (
+    //aqui tambien puedo escribir un poco de logica
+    <section className="text-center">
+      {/*aqui va todo el maquetado*/}
+      <h1 className="display-4">Proyecto contador con react</h1>
+      <h2 className="display-6">Este es un subtitulo</h2>
+      <hr />
+    </section>
+  );
+};
+
+export default Titulos;
